9d5363cf8ef0dc393556d8e6243ffda8c9a2d85f
Juan Pablo Guerrero <jp3_4@hotmail.com>
2023-02-20 18:11:39 -0500

    Update schema.sql
    Add table with the required columns

--- schema.sql
+++ schema.sql
@@ -1,5 +1,3 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals(id integer, name varchar(20), date_of_birth date, escape_attempts integer, neutered boolean, weight_Kg decimal);
