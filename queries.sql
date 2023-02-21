d45f792fdd1e9b5e06222d411d0f5aa63e8736a1
Juan Pablo Guerrero <jp3_4@hotmail.com>
2023-02-20 19:19:12 -0500

    Update queries.sql
    Add all the required queries to get info from table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+select * from animals where name like '%mon';
+select * from animals where date_of_birth > '2016-01-01' and  date_of_birth < '2019-12-31' ;
+select * from animals where neutered = true and escape_attempts < 3;
+select date_of_birth from animals where name = 'Agumon' or name = 'Pikachu';
+select name, escape_attempts from animals where weight_Kg > 10.5;
+select * from animals where neutered = true;
+select * from animals where name != 'Gabumon';
+select * from animals where weight_Kg >=10.4 and  weight_Kg <=17.3;
