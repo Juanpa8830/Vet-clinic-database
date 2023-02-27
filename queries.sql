28033d47493f0dbb99fe8c118c4e0405df571376
Juan Pablo Guerrero <jp3_4@hotmail.com>
2023-02-27 10:52:35 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,8 +1,8 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 select * from animals where name like '%mon';
-select * from animals where date_of_birth > '2016-01-01' and  date_of_birth < '2019-12-31' ;
-select * from animals where neutered = true and escape_attempts < 3;
+select name from animals where date_of_birth > '2015-12-31' and date_of_birth < '2020-01-01' ;
+select name from animals where neutered = true and escape_attempts < 3;
 select date_of_birth from animals where name = 'Agumon' or name = 'Pikachu';
 select name, escape_attempts from animals where weight_Kg > 10.5;
 select * from animals where neutered = true;
