4f80f5dc4b84a3e5775304dc1a92b3dd0f897ddf
Juanpa8830 <jp3_4@hotmail.com>
2023-03-02 20:53:52 -0500

    Create new tables vets/specializations/visits

--- schema.sql
+++ schema.sql
@@ -11,3 +11,7 @@ alter table animals add column species_id integer;
 alter table animals add constraint fk_constraint foreign key(species_id) references species(id);
 alter table animals add column owner_id integer;
 alter table animals add constraint fk_constraint2 foreign key(owner_id) references owners(id);  
+
+create table vets(id int generated always as identity, name varchar(50), age integer, date_of_graduation date, primary key(id));
+create table specializations(specie_id int references species(id), vet_id int references vets(id), primary key(specie_id, vet_id));
+create table visits (animal_id int references animals(id), vets_id int references vets(id), date_of_visit date, primary key(animal_id, vets_id, date_of_visit));
